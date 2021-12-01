794f2f0481210dd650b0b4c93e6f20affb12b5b5
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2021-12-01 09:41:31 +0100

    Day one part two

--- src/main/kotlin/be/moac/aoc2021/day01/CalculateDepth.kt
+++ src/main/kotlin/be/moac/aoc2021/day01/CalculateDepth.kt
@@ -3,24 +3,24 @@ package be.moac.aoc2021.day01
 import be.moac.aoc2021.readLines
 
 fun main() {
-    val input: List<Long> = "/day01_input.txt".readLines { i -> i.toLong()}
-    val result = CalculateDepth partOne   input
+    val input: List<Long> = "/day01_input.txt".readLines { i -> i.toLong() }
 
-    println(result)
+    println("Part one: ${CalculateDepth partOne input}")
+    println("Part two: ${CalculateDepth partTwo input}")
 }
 
 object CalculateDepth {
 
     infix fun partOne(input: List<Long>): Long =
-        input
-        .fold(Pair<Long, Long?>(0L, null))
-        { acc, l -> acc.second?.let { if (l > it) (acc.first.plus(1L) to l) else (acc.first to l) }
-            ?: (0L to l) }
-            .first
+        input.zipWithNext().fold(0)
+        { acc, l -> if (l.first < l.second) acc.plus(1L) else acc }
 
-    infix fun partTwo(input: List<Long>): Long {
 
-        return 0L
-    }
+    infix fun partTwo(input: List<Long>): Long =
+        partOne((0..input.size - 3)
+            .map { input.drop(it).sum() }
+            .toList())
+
+    private fun List<Long>.sum() = this[0].plus(this[1]).plus(this[2])
 
 }
--- src/test/kotlin/be/moac/aoc2021/day01/CalculateDepthTest.kt
+++ src/test/kotlin/be/moac/aoc2021/day01/CalculateDepthTest.kt
@@ -14,4 +14,13 @@ internal class CalculateDepthTest {
 
         assertThat(result).isEqualTo(7L)
     }
+
+    @Test
+    internal fun partTwo() {
+        val input = listOf(199L, 200L, 208L, 210L, 200L, 207L, 240L, 269L, 260L, 263L)
+
+        val result = CalculateDepth partTwo input
+
+        assertThat(result).isEqualTo(5L)
+    }
 }
