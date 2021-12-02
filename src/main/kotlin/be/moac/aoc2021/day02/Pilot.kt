f13a97ff5506204a73a797d014334f623fd99ef7
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2021-12-02 09:15:25 +0100

    Day two part two

--- src/main/kotlin/be/moac/aoc2021/day02/Pilot.kt
+++ src/main/kotlin/be/moac/aoc2021/day02/Pilot.kt
@@ -26,9 +26,25 @@ object Pilot {
         }
 
 
-    infix fun partTwo(input: List<String>): Long {
-        return 0L
-    }
+    infix fun partTwo(input: List<String>) =
+        input.fold(Position()) { position, s ->
+            with(s.split(" ")) {
+                when (this.first()) {
+                    "forward" -> position.forward(this.last().toLong())
+                    "down" -> position.down(this.last().toLong())
+                    "up" -> position.up(this.last().toLong())
+                    else -> position
+                }
+            }
+        }.result()
 
+    data class Position(val horizontal: Long = 0L, val depth: Long = 0L, val aim: Long = 0L) {
+
+        fun down(x: Long) = Position(horizontal = horizontal, depth = depth, aim = aim.plus(x))
+        fun up(x: Long) = Position(horizontal = horizontal, depth = depth, aim = aim.minus(x))
+        fun forward(x: Long) = Position(horizontal = horizontal.plus(x), depth = depth.plus(aim.times(x)), aim = aim)
+
+        fun result() = horizontal.times(depth)
+    }
 
 }
--- src/test/kotlin/be/moac/aoc2021/day02/PilotTest.kt
+++ src/test/kotlin/be/moac/aoc2021/day02/PilotTest.kt
@@ -20,4 +20,20 @@ internal class PilotTest {
 
         assertThat(result).isEqualTo(150L)
     }
+
+    @Test
+    internal fun `part two`() {
+        val input = listOf(
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2"
+        )
+
+        val result = Pilot partTwo  input
+
+        assertThat(result).isEqualTo(900L)
+    }
 }
