296b438bdcb40a65fc22fb1301f17007e824d30a
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2021-12-06 10:37:43 +0100

    Day 6

--- src/main/kotlin/be/moac/aoc2021/Day06.kt
+++ src/main/kotlin/be/moac/aoc2021/Day06.kt
@@ -10,11 +10,50 @@ fun main() {
 object Day06 {
 
     infix fun partOne(input: List<String>): Long {
-        return 0L
+        val fish = input.parse()
+        fun cycle(fish:List<Fish>, index: Int):List<Fish> {
+            return when(index) {
+                80 -> fish
+                else -> {
+                    val newFish = fish.filter { it.timer == 0 }.map { Fish(8) }
+                    val oldFish = fish.map { it.handle() }
+                    cycle(listOf(*oldFish.toTypedArray(), *newFish.toTypedArray()), index.plus(1))
+                }
+            }
+        }
+
+        return cycle(fish, 0).count().toLong()
     }
 
     infix fun partTwo(input: List<String>): Long {
-      return 0L
+        val fish = input.first().split(",").map { it.toInt() }.groupBy { it }.map { (k,v) -> k to v.size.toLong() }.toMap()
+
+        fun cycle(fish: Map<Int, Long>, index: Int): Long {
+//            println("$index - ${fish}")
+            return when(index){
+                256 -> fish.values.sum()
+                else -> {
+                    val oldFish = fish.map { (age, amount) -> when{
+                        age == 0 -> 6 to amount + (fish[7]?:0)
+                        age == 7 -> 6 to amount + (fish[0]?:0)
+                        else -> age.minus(1) to amount
+                    }}.toMap() + mapOf(8 to (fish[0]?:0L))
+
+                    cycle(oldFish, index.plus(1))
+                }
+            }
+        }
+        return cycle(fish, 0)
     }
 
+
+    data class Fish(val timer: Int) {
+        fun handle():Fish = when {
+            timer > 0 -> Fish(timer.minus(1))
+            else -> Fish(6)
+        }
+
+    }
+
+    private fun List<String>.parse() = this.first().split(",").map { Fish(it.toInt()) }
 }
---
+++ src/test/kotlin/be/moac/aoc2021/Day06Test.kt
@@ -0,0 +1,27 @@
+package be.moac.aoc2021
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day06Test {
+
+    @Test
+    internal fun `part one`() {
+
+        val input = listOf("3,4,3,1,2")
+
+        val result = Day06 partOne input
+
+        assertThat(result).isEqualTo(5934L)
+    }
+
+    @Test
+    internal fun `part two`() {
+
+        val input = listOf("3,4,3,1,2")
+
+        val result = Day06 partTwo input
+
+        assertThat(result).isEqualTo(26984457539L)
+    }
+}
