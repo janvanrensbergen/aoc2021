01c9fd1589cbf30be3407ff953a7c62ae4ca5c96
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2021-12-10 11:17:22 +0100

    Day 10 part two

--- src/main/kotlin/be/moac/aoc2021/Day10.kt
+++ src/main/kotlin/be/moac/aoc2021/Day10.kt
@@ -13,17 +13,42 @@ fun main() {
 }
 
 object Day10 {
+    private val mapping = mapOf('(' to ')', '[' to ']', '<' to '>', '{' to '}')
 
     infix fun partOne(input: List<CharArray>): Int  {
         val points = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
         return input.mapNotNull { it.isCorrupt() }.sumOf { points[it]!! }
     }
 
-    infix fun partTwo(input: List<CharArray>): Int = 0
+    infix fun partTwo(input: List<CharArray>): Long {
+        val sorted = input.filter { it.isCorrupt() == null }
+            .map { it.complete() }
+            .map { it.calculatePoints() }
+            .sorted()
+
+        return sorted[sorted.size/2]
+    }
+
+    fun String.calculatePoints(): Long {
+        val points = mapOf(')' to 1L, ']' to 2L, '}' to 3L, '>' to 4L)
+        return this.fold(0L) { acc, c ->
+            acc.times(5L).plus(points[c] ?: 0L)
+        }
+    }
+
+    fun CharArray.complete(): String {
+        val stack = ArrayDeque<Char>()
+        this.forEach { c ->
+            when {
+                c.isOpening() -> stack.push(c)
+                else -> stack.pop()
+            }
+        }
+        return stack.map { mapping[it]!! }.joinToString(separator = "")
+    }
 
 
     fun CharArray.isCorrupt(): Char? {
-        val mapping = mapOf('(' to ')', '[' to ']', '<' to '>', '{' to '}')
         val stack = ArrayDeque<Char>()
         return this.firstOrNull {
             when {
@@ -31,10 +56,8 @@ object Day10 {
                     stack.push(it);
                     false
                 }
-                !it.isOpening() -> {
-                    mapping[stack.pop()] != it
-                }
-                else -> TODO()
+                !it.isOpening() -> mapping[stack.pop()] != it
+                else -> false
             }
         }
 
--- src/test/kotlin/be/moac/aoc2021/Day10Test.kt
+++ src/test/kotlin/be/moac/aoc2021/Day10Test.kt
@@ -1,5 +1,7 @@
 package be.moac.aoc2021
 
+import be.moac.aoc2021.Day10.calculatePoints
+import be.moac.aoc2021.Day10.complete
 import be.moac.aoc2021.Day10.isCorrupt
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -30,11 +32,22 @@ internal class Day10Test {
     @Test
     fun `part two`() {
 
-        val input = listOf<CharArray>()
+        val input = listOf(
+            "[({(<(())[]>[[{[]{<()<>>".toCharArray(),
+            "[(()[<>])]({[<{<<[]>>(".toCharArray(),
+            "{([(<{}[<>[]}>{[]{[(<()>".toCharArray(),
+            "(((({<>}<{<{<>}{[]{[]{}".toCharArray(),
+            "[[<[([]))<([[{}[[()]]]".toCharArray(),
+            "[{[{({}]{}}([{[{{{}}([]".toCharArray(),
+            "{<[[]]>}<{[{[{[]{()[[[]".toCharArray(),
+            "[<(<(<(<{}))><([]([]()".toCharArray(),
+            "<{([([[(<>()){}]>(<<{{".toCharArray(),
+            "<{([{{}}[<[[[<>{}]]]>[]]".toCharArray(),
+        )
 
         val result = Day10 partTwo input
 
-        assertThat(result).isEqualTo(26397)
+        assertThat(result).isEqualTo(288957)
     }
 
 
@@ -43,13 +56,28 @@ internal class Day10Test {
         
         assertThat("{([(<{}[<>[]}>{[]{[(<()>".toCharArray().isCorrupt()).isEqualTo('}')
         assertThat("[[<[([]))<([[{}[[()]]]".toCharArray().isCorrupt()).isEqualTo(')')
-
         assertThat("[{[{({}]{}}([{[{{{}}([]".toCharArray().isCorrupt()).isEqualTo(']')
         assertThat("[<(<(<(<{}))><([]([]()".toCharArray().isCorrupt()).isEqualTo(')')
         assertThat("<{([([[(<>()){}]>(<<{{".toCharArray().isCorrupt()).isEqualTo('>')
-
         assertThat("[({(<(())[]>[[{[]{<()<>>".toCharArray().isCorrupt()).isNull()
+    }
 
+    @Test
+    fun `complete line`() {
+        assertThat("[({(<(())[]>[[{[]{<()<>>".toCharArray().complete()).isEqualTo("}}]])})]")
+        assertThat("[(()[<>])]({[<{<<[]>>(".toCharArray().complete()).isEqualTo(")}>]})")
+        assertThat("(((({<>}<{<{<>}{[]{[]{}".toCharArray().complete()).isEqualTo("}}>}>))))")
+        assertThat("{<[[]]>}<{[{[{[]{()[[[]".toCharArray().complete()).isEqualTo("]]}}]}]}>")
+        assertThat("<{([{{}}[<[[[<>{}]]]>[]]".toCharArray().complete()).isEqualTo("])}>")
+    }
+
+    @Test
+    fun `calculate total points`() {
+        assertThat("}}]])})]".calculatePoints()).isEqualTo(288957)
+        assertThat(")}>]})".calculatePoints()).isEqualTo(5566)
+        assertThat("}}>}>))))".calculatePoints()).isEqualTo(1480781)
+        assertThat("]]}}]}]}>".calculatePoints()).isEqualTo(995444)
+        assertThat("])}>".calculatePoints()).isEqualTo(294)
     }
 }
 
