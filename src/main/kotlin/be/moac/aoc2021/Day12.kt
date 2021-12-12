0c8c11d06e7d5caf3764b3f80434ee60bfedcedc
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2021-12-13 00:02:54 +0100

    Day 12 refactor

--- src/main/kotlin/be/moac/aoc2021/Day12.kt
+++ src/main/kotlin/be/moac/aoc2021/Day12.kt
@@ -7,7 +7,7 @@ fun main() {
     val input: List<String> = "/day12_input.txt".readLines()
 
     println("Part one: ${timed { Day12 partOne input }}")
-    println("Part two: ${timed(1) { Day12 partTwo input }}")
+    println("Part two: ${timed(10) { Day12 partTwo input }}")
 
 }
 
@@ -16,65 +16,42 @@ object Day12 {
     infix fun partOne(input: List<String>): Long {
         val graph = input.parse()
 
-        fun traverse(x: String, path: Path, visited: List<String> = listOf()): List<Path> {
-            return when (x) {
-                "end" -> listOf(listOf(*path.toTypedArray(), x))
-                else -> {
-                    val newPath = listOf(*path.toTypedArray(), x)
-                    val newVisited = visited.add(x)
-
-                    val result = graph.filter { it.start == x }
-                        .filterNot { newVisited.contains(it.end) }
-                        .flatMap { traverse(it.end, newPath, newVisited) }
-
-                    result.ifEmpty { listOf(newPath) }
-                }
+        fun traverse(x: String, path: Path = "", visited: List<String> = listOf()): List<Path> =
+            when {
+                x == "end" -> listOf(path + x)
+                visited.contains(x) -> listOf(path)
+                else -> graph.filter { it.start == x }.flatMap { traverse(it.end, path + x, visited.add(x)) }
             }
-        }
 
-        return traverse("start", listOf()).filter { it.contains("end") }.size.toLong()
+        return traverse("start").filter { it.contains("end") }.size.toLong()
     }
 
     infix fun partTwo(input: List<String>): Long {
         val graph = input.parse()
 
-        fun traverse(x: String, path: Path, visited: Map<String, Int> = mapOf()): List<Path> {
-            return when (x) {
-                "end" -> listOf(listOf(*path.toTypedArray(), x))
-                else -> {
-                    val newPath = listOf(*path.toTypedArray(), x)
-                    val newVisited = visited.add(x)
-
-                    val result = graph.filter { it.start == x }
-                        .filterNot {
-                            newVisited.any { v -> v.value == 2 } &&
-                            (newVisited[it.end] ?: 0) > 0 }
-                        .flatMap { traverse(it.end, newPath, newVisited) }
-
-                    result.ifEmpty { listOf(newPath) }
-                }
+        fun traverse(x: String, path: Path = "", visited: Map<String, Int> = mapOf()): List<Path> =
+            when {
+                x == "end" -> listOf(path + x)
+                visited.any { v -> v.value == 2 } && (visited[x] ?: 0) > 0 -> listOf(path)
+                else -> graph.filter { it.start == x }.flatMap { traverse(it.end, path + x, visited.add(x)) }
             }
-        }
 
-        return traverse("start", listOf())
-            .filter { it.contains("end") }
-            .size.toLong()
+        return traverse("start").filter { it.contains("end") }.size.toLong()
     }
 
 
     private fun List<String>.add(x: String) = if (x.all { it.isLowerCase() }) {
-        listOf(*this.toTypedArray(), x)
+        this + x
     } else {
         this
     }
 
-    private fun Map<String, Int>.add(x: String): Map<String, Int> = if (x.all { it.isLowerCase() }) {
-        val map = this.toMutableMap()
-        map[x] = this[x]?.let { it+1 } ?: 1
-        map
-    } else {
-        this
-    }
+    private fun Map<String, Int>.add(x: String): Map<String, Int> =
+        if (x.all { it.isLowerCase() }) {
+            this + (x to (this[x]?.let { it + 1 } ?: 1))
+        } else {
+            this
+        }
 
     fun List<String>.parse(): List<Connection> =
         this.flatMap {
@@ -92,6 +69,6 @@ object Day12 {
 
 }
 
-private typealias Path = List<String>
+private typealias Path = String
 
 
